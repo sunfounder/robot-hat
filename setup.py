cb5f1d64082952fedc637478f6d681620b8369e0
Star152 <1972133159@qq.com>
2021-08-10 17:12:29 +0800

    install pyaudio modul

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['RPi.GPIO', 'smbus', 'spidev', 'pyserial', 'pillow'],
+    install_requires=['RPi.GPIO', 'smbus', 'spidev', 'pyserial', 'pillow',"pyaudio"],
     
     
     
