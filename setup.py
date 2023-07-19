b1d45ae2eefbf25cf5ada96988ca9b500dd0c721
Cavon <cavonxx@gmail.com>
2023-07-19 15:04:02 +0800

    add smbus dependency

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['RPi.GPIO', 'spidev', 'pyserial' ],
+    install_requires=['RPi.GPIO', 'spidev', 'pyserial', 'smbus' ],
     
     
     # To provide executable scripts, use entry points in preference to the
