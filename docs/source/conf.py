2f61138e3935dc2bfe0406ffd72e0ff15d6c3ead
zhuang <zhuang201704@gmail.com>
2023-12-19 15:22:55 +0800

    Added Japanese and German

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,6 +43,8 @@ extensions = [
     'sphinx.ext.imgmath',
 ]
 
+latex_engine = 'xelatex'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
