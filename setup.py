f8261b1d73c95cf1918ce4fd8444068dc9423f48
Lovmong <1967471283@qq.com>
2024-01-16 10:28:51 +0800

    change method of config

--- setup.py
+++ setup.py
@@ -1,86 +1,20 @@
 #!/usr/bin/env python3
-
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
 import sys
-import os
+from setuptools import setup
 import time
 import threading
 
-sys.path.append('./robot_hat')
-from version import __version__
+# if len(sys.argv) > 1 and sys.argv[1] == 'install':
+#     tip = '''\
+# "setup.py install" is deprecated.Please use the following to install:
+#         sudo python3 install.py
+# '''
+#     print(f'{tip}')
+#     sys.exit(1)
+# else:
+#     setup()
 
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the relevant file
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
-setup(
-    name='robot_hat',
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version=__version__,
-
-    description='Library for SunFounder Robot Hat',
-    long_description=long_description,
-
-    # The project's main homepage.
-    url='https://github.com/sunfounder/robot-hat/tree/v2.0',
-
-    # Author details
-    author='SunFounder',
-    author_email='service@sunfounder.com',
-
-    # Choose your license
-    license='GNU',
-    zip_safe=False,
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
-    ],
-
-    # What does your project relate to?
-    keywords='python raspberry pi GPIO sunfounder',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['tests', 'docs']),
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],
-    
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        'console_scripts': [
-            'robot_hat=robot_hat:__main__',
-        ],
-    },
-)
+setup()
 
 def run_command(cmd=""):
     import subprocess
@@ -131,29 +65,27 @@ def do(msg="", cmd=""):
         errors.append("%s error:\n  Status:%s\n  Error:%s" %
                       (msg, status, result))
 
-
 APT_INSTALL_LIST = [
     'raspi-config',
-    'python3-smbus',
     "i2c-tools",
     "espeak",
-    "python3-pyaudio",
     'libsdl2-dev',
     'libsdl2-mixer-dev',
 ]
 
 PIP_INSTALL_LIST = [
-    'RPi.GPIO',
+    'smbus2',
+    'gpiozero',
+    'pyaudio',
     'spidev',
     'pyserial',
-    "gpiozero",
     'pillow',
     "'pygame>=2.1.2'",
 ]
 
 if sys.argv[1] == 'install':
     try:
-    # Install dependency 
+        # Install dependency 
         # =============================
         print("Install dependencies with apt:")
         # update apt
@@ -211,4 +143,5 @@ if sys.argv[1] == 'install':
     finally:
         sys.stdout.write('\033[?25h') # cursor visible 
         sys.stdout.flush()
-                
+
+
