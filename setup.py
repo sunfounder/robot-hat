2068e4cab4a510faed665033dadcc649b7fa4b41
Lovmong <1967471283@qq.com>
2024-02-23 09:45:20 +0800

    fix spelling errors

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ here = path.abspath(path.dirname(__file__))
 # with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 #     long_description = f.read()
 
-avaiable_options = ["--no-dep",]
+avaiable_options = ["--no-dep", "--only-library"]
 options = []
 if len(sys.argv) > 1:
     options = list.copy(sys.argv[1:])
@@ -159,6 +159,9 @@ PIP_INSTALL_LIST = [
 ]
 
 def install():
+    if "--only-library" in options:
+       return
+
     try:
         # Install dependency 
         # =============================
@@ -176,7 +179,7 @@ def install():
             do(msg="install pico2wave",
                 cmd='wget http://ftp.us.debian.org/debian/pool/non-free/s/svox/libttspico0_1.0+git20130326-9_armhf.deb'
                 +' && wget http://ftp.us.debian.org/debian/pool/non-free/s/svox/libttspico-utils_1.0+git20130326-9_armhf.deb'
-                +' && sudo apt-get-get install -f ./libttspico0_1.0+git20130326-9_armhf.deb ./libttspico-utils_1.0+git20130326-9_armhf.deb -y')
+                +' && sudo apt-get install -f ./libttspico0_1.0+git20130326-9_armhf.deb ./libttspico-utils_1.0+git20130326-9_armhf.deb -y')
             # --------------------------------
             print("Install dependencies with pip3:")
             # check whether pip has the option "--break-system-packages"
