af24005ab9925b416d902880a7ad91c32deceb83
Lovmong <1967471283@qq.com>
2024-09-13 13:49:38 +0800

    add command to toggle the speaker

--- robot_hat/__init__.py
+++ robot_hat/__init__.py
@@ -17,15 +17,28 @@ from .robot import Robot
 from .version import __version__
 
 def __usage__():
-    print("Usage: robot_hat [reset_mcu]")
+    print('''
+    Usage: robot_hat [option]
+
+    reset_mcu               reset mcu on robot-hat
+    enable_speaker          enable speaker (drive high gpio 20)
+    disable_speaker         disable speaker (drive low gpio 20)
+    ''')
     quit()
 
 
 def __main__():
     import sys
+    import os
     if len(sys.argv) == 2:
         if sys.argv[1] == "reset_mcu":
             reset_mcu()
             print("Onboard MCU reset.")
+        elif sys.argv[1] == "enable_speaker":
+            print("Enable Speaker.")
+            os.popen("pinctrl set 20 op dh")
+        elif sys.argv[1] == "disable_speaker":
+            print("Enable Speaker.")
+            os.popen("pinctrl set 20 op dl")
     else:
         __usage__()
